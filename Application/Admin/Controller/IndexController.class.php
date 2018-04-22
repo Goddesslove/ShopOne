6c1eecd4e5beac588c25823d3c8c71587b6e498d
Goddesslove <599971739@qq.com>
2018-04-22 18:11:41 +0800

    商城系统One

---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,22 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+namespace Admin\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+
+	 $this->display();
+	   }
+    public function head(){
+
+        $this->display();
+    }
+    public function left(){
+
+        $this->display();
+    }
+    public function right(){
+
+        $this->display();
+    }
+}
